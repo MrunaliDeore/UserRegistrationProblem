1336ea554edbe3a63ae4b731b3dd3392d64f7665
MrunaliDeore <mrunalideoreohk1@gmail.com>
2022-07-28 22:01:47 +0530

    Added : main class file added - just simple msg

---
+++ MainClass.java
@@ -0,0 +1,11 @@
+package Nineteen;
+
+public class MainClass {
+	
+	public static void main(String[] args){
+		
+		System.out.println("Assignment Day 19\n");
+		System.out.println("User Registration\nUser Registration System needs to ensure all validations are in place during the User Entry");
+	}
+
+}
